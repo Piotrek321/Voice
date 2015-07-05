407c43ce12960814187bfb2beadc1ab6606f7e1d
Piotr Belkner <piotrbelkner@gmail.com>
2015-07-05 16:46:21 +0200

    AA

--- catkin_ws/src/beginner_tutorials/scripts/GetIntentViaSpeech.py
+++ catkin_ws/src/beginner_tutorials/scripts/GetIntentViaSpeech.py
@@ -3,37 +3,53 @@ import pycurl
 import cStringIO
 import json
 
+import os
+import wave
 
-
-import requests
 import urllib
+import cStringIO
+import alsaaudio
+
+
 
 key = 'THAKYMA767MXNDXJOZXDUBGHGFIRW5ER'
 
-def query(query):
-    headers = {'Authorization': 'Bearer ' + key}
-    url = 'https://api.wit.ai/message?q=' + urllib.quote(query)
-    return requests.get(url, headers = headers)
+curl_header = ["Authorization: Bearer THAKYMA767MXNDXJOZXDUBGHGFIRW5ER",
+                 "Content-Type: audio/wav"]
+curl_url = 'https://api.wit.ai/speech?v=20150626'
 
-def queryAudio(filename):
-    wav_file = open(filename, 'rb')
-    headers = {'Authorization': 'Bearer ' + key, 'Content-Type': 'audio/wav'}
-    url =  'https://api.wit.ai/speech'
-    data = wav_file
-    r = requests.post(url, headers = headers, data = data)
-    wav_file.close()
-    return r
+postdict = [ ('userfile', open('tekst.wav', 'rb').read()) ]
+
+c = pycurl.Curl()
+c.setopt(pycurl.URL, curl_url)
+c.setopt(pycurl.HTTPHEADER,curl_header)
+print"ABC"
+c.setopt(c.HTTPPOST,postdict)
+
+#c.setopt(c.WRITEDATA, )
+c.perform()
+c.close()
 
 
 
-jsonResponse = queryAudio('tekst.wav').text
-dataDict = json.loads(jsonResponse)
-print (dataDict)
-intent= dataDict["outcomes"][0]["intent"]
-print (intent)
+
+
+
+
+
+
+
 
 """
-key = 'THAKYMA767MXNDXJOZXDUBGHGFIRW5ER'
+
+
+
+#c.setopt(c.HTTPPOST,[("data-binary", (c.FORM_FILE, "tekst.wav"))])
+c.setopt(c.POSTFIELDS, '@tekst.wav')
+c.setopt(c.VERBOSE, 1)
+#c.setopt(c.WRITEFUNCTION, response.write)
+c.perform()
+c.close()
 
 def query(query):
     headers = {'Authorization': 'Bearer ' + key}
@@ -48,6 +64,4 @@ def queryAudio(filename):
     r = requests.post(url, headers = headers, data = data)
     wav_file.close()
     return r
-
-
 """
---
+++ catkin_ws/src/beginner_tutorials/scripts/wysylanieDzwiekiWit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import requests
+import json
+
+import urllib3
+urllib3.disable_warnings()
+
+key = 'THAKYMA767MXNDXJOZXDUBGHGFIRW5ER'
+
+def query(query):
+    headers = {'Authorization': 'Bearer ' + key}
+    url = 'https://api.wit.ai/message?q=' + urllib.quote(query)
+    return requests.get(url, headers = headers)
+
+def queryAudio(filename):
+    wav_file = open(filename, 'rb')
+    headers = {'Authorization': 'Bearer ' + key, 'Content-Type': 'audio/wav'}
+    url =  'https://api.wit.ai/speech?v=20141022'
+    data = wav_file
+    r = requests.post(url, headers = headers, data = data)
+    wav_file.close()
+    return r
+
+b = queryAudio('tekst.wav').text
+
+
+a= json.loads(b,encoding = 'iso-8859-2')
+print a
+
+encoded_str = a["outcomes"][0]["_text"].encode("'iso-8859-2")
+print encoded_str
